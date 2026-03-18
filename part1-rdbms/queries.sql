e482e51ad15cceb98a87aac3ca56c13d67edc642
yashvirchandra <yashvirchandra@gmail.com>
2026-03-18 17:39:39 +0530

    Added SQL queries, completed Q1.3.
    COMPLETED THE Q1.3.  
    AND IT'S FOLLOWING QUERIES :-
    -- Q1: List all customers from Mumbai along with their total order value
    -- Q2: Find the top 3 products by total quantity sold
    -- Q3: List all sales representatives and the number of unique customers they have handled
    -- Q4: Find all orders where the total value exceeds 10,000, sorted by value descending
    -- Q5: Identify any products that have never been ordered

--- part1-rdbms/queries.sql
+++ part1-rdbms/queries.sql
@@ -1,1 +1,69 @@
+-- Q1.3. SQL QUERIES :-
+-- =========================
+-- Q1: List all customers from Mumbai along with their total order value.
+-- =========================
+SELECT 
+    c.customer_name,
+    SUM(od.quantity * p.unit_price) AS total_spent
+FROM customers c
+JOIN orders o 
+    ON c.customer_id = o.customer_id
+JOIN order_details od 
+    ON o.order_id = od.order_id
+JOIN products p 
+    ON od.product_id = p.product_id
+WHERE c.customer_city = 'Mumbai'
+GROUP BY c.customer_name;
 
+
+-- =========================
+-- Q2: Find the top 3 products by total quantity sold.
+-- =========================
+SELECT 
+    p.product_name,
+    SUM(od.quantity) AS total_sold
+FROM products p
+JOIN order_details od 
+    ON p.product_id = od.product_id
+GROUP BY p.product_name
+ORDER BY total_sold DESC
+LIMIT 3;
+
+
+-- =========================
+-- Q3: List all sales representatives and the number of unique customers they have handled.
+-- =========================
+SELECT 
+    sr.sales_rep_name,
+    COUNT(DISTINCT o.customer_id) AS total_customers
+FROM sales_reps sr
+JOIN orders o 
+    ON sr.sales_rep_id = o.sales_rep_id
+GROUP BY sr.sales_rep_name;
+
+
+-- =========================
+-- Q4: Find all orders where the total value exceeds 10,000, sorted by value descending.
+-- =========================
+SELECT 
+    o.order_id,
+    SUM(od.quantity * p.unit_price) AS total_value
+FROM orders o
+JOIN order_details od 
+    ON o.order_id = od.order_id
+JOIN products p 
+    ON od.product_id = p.product_id
+GROUP BY o.order_id
+HAVING total_value > 10000
+ORDER BY total_value DESC;
+
+
+-- =========================
+-- Q5: Identify any products that have never been ordered.
+-- =========================
+SELECT 
+    p.product_name
+FROM products p
+LEFT JOIN order_details od 
+    ON p.product_id = od.product_id
+WHERE od.product_id IS NULL;
